d701f77a025dac21ea21db070d6391ef9149a492
qiaowanze <l>
2020-07-07 15:58:19 +0800

    first commit

---
+++ docs/.vuepress/config/nav/index.js
@@ -0,0 +1,11 @@
+module.exports = [
+  { text: 'Home', link: '/', icon: 'reco-home' },
+  { text: 'TimeLine', link: '/timeline/', icon: 'reco-date' },
+  { text: 'Contact', 
+    icon: 'reco-message',
+    items: [
+      { text: 'GitHub', link: 'https://github.com/qiaowanze', icon: 'reco-github' },
+      { text: '博客圆', link: 'https://www.cnblogs.com/qiaowanze/', icon: 'reco-bokeyuan' },
+    ]
+  }
+]
